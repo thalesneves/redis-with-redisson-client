5654133e57a5afcce90ced79df73422d1fc585b6
Thales Neves <thales_neves97@hotmail.com>
2024-03-26 17:06:21 -0300

    first commit

--- src/main/java/com/example/demo/configuration/RedissonConfig.java
+++ src/main/java/com/example/demo/configuration/RedissonConfig.java
@@ -5,7 +5,6 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebServerApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,8 +15,6 @@ public class RedissonConfig {
     @Value("${REDIS_PASSWORD}")
     private String redisPassword;
 
-//    private RedissonClient redissonClient;
-
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
@@ -25,8 +22,6 @@ public class RedissonConfig {
                 .setAddress("redis://localhost:6379")
                 .setPassword(redisPassword);
 
-//        redissonClient = Redisson.create(config);
-//        return redissonClient;
         return Redisson.create(config);
     }
 
@@ -39,26 +34,5 @@ public class RedissonConfig {
     public TesteThales testeThales(RedissonClient redissonClient) {
         return new TesteThales(redissonClient);
     }
-
-//    public void recreateRedissonClient() {
-//        Config config = new Config();
-//        config.useSingleServer()
-//                .setAddress("redis://localhost:6379")
-//                .setPassword(redisPassword);
-//
-//        redissonClient = Redisson.create(config);
-//
-//        System.out.println(redissonClient);
-//    }
-//
-//    public RedissonClient getRedissonClient(String redisPassword) {
-//        this.redisPassword = redisPassword;
-//
-//        if (redissonClient == null || redissonClient.isShutdown()) {
-//            recreateRedissonClient();
-//        }
-//
-//        return redissonClient;
-//    }
 
 }
--- src/main/java/com/example/demo/controller/RedisController.java
+++ src/main/java/com/example/demo/controller/RedisController.java
@@ -1,17 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.configuration.RedissonConfig;
 import com.example.demo.configuration.TesteThales;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.WriteRedisConnectionException;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.redis.connection.RedisInvalidSubscriptionException;
-import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +22,7 @@ public class RedisController {
     @Autowired
     private ApplicationContext applicationContext;
 
-//    private final RedissonConfig redissonConfig = new RedissonConfig();
+    private int i = 0;
 
     @Autowired
     public RedisController(RedissonClient redissonClient, TesteThales testeThales) {
@@ -37,16 +33,19 @@ public class RedisController {
     @PostMapping("/inserirNoRedis")
     public String inserirNoRedis(@RequestBody String dados) {
         try {
-            RedissonClient redissonClientThales = testeThales.getRedissonClient();
-
             redissonClient.getBucket("chave").set(dados);
 
             return "Dados inseridos com sucesso no Redis.";
         } catch (WriteRedisConnectionException e) {
+            String[] pass = {"admin_thales", "admin_jarvis"};
+
             Config config = new Config();
             config.useSingleServer()
                     .setAddress("redis://localhost:6379")
-                    .setPassword("admin_thales");
+                    .setPassword(pass[i]);
+            i++;
+
+            if (i == 1) i = 0;
 
             RedissonClient newRedissonClient = Redisson.create(config);
 
@@ -54,11 +53,8 @@ public class RedisController {
             registry.destroySingleton("redissonClient");
             registry.registerSingleton("redissonClient", newRedissonClient);
 
-            this.redissonClient = newRedissonClient;
-            inserirNoRedis("");
+            return "Tente de novo!";
         }
-
-        return null;
     }
 
 }
